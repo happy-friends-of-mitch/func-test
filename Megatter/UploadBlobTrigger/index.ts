812169f5668feb2b87ceb243a6a3cfbec81ee86f
smaru1111 <marusou20@gmail.com>
2023-07-16 11:23:56 +0900

    update UploadBlobTrigger

--- Megatter/UploadBlobTrigger/index.ts
+++ Megatter/UploadBlobTrigger/index.ts
@@ -1,80 +1,40 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { ContainerClient, HttpRequestBody } from "@azure/storage-blob";
-
-const { BlobServiceClient } = require("@azure/storage-blob");
-const { DefaultAzureCredential } = require('@azure/identity');  
-const { v1: uuidv1 } = require("uuid");
-// require("dotenv").config();
-
-
-// blobにbase64の画像データをアップロードする
-async function uploadBlob(value:HttpRequestBody) {
-  try {
-    console.log("Azure Blob storage v12 - JavaScript quickstart sample");
-    const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
-    const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
-    const AZURE_STORAGE_CONTAINER_NAME = process.env.AZURE_STORAGE_CONTAINER_NAME;
-    
-    if (!accountName) throw Error('Azure Storage accountName not found');
-
-    if (!AZURE_STORAGE_CONNECTION_STRING) {
-      throw Error('Azure Storage Connection string not found');
-    }
-    if (!AZURE_STORAGE_CONTAINER_NAME) {
-      throw Error('Azure Storage Container name not found');
-    }
-
-    // Create the BlobServiceClient object with connection string
-    const blobServiceClient = BlobServiceClient.fromConnectionString(
-      AZURE_STORAGE_CONNECTION_STRING
-    );
-  
-    const containerName:String = AZURE_STORAGE_CONTAINER_NAME;  
-  
-    console.log('\nCreating container...');
-    console.log('\t', containerName);
-  
-    // コンテナへの参照を取得する。
-    const containerClient:ContainerClient = blobServiceClient.getContainerClient(containerName);
-
-    // blobのコンテナをアップロード
-    // Create a unique name for the blob
-    const blobName:string = uuidv1() + ".jpeg";
-  
-    // Get a block blob client
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-  
-    // Display blob name and url
-    console.log(
-      `\nUploading to Azure storage as blob\n\tname: ${blobName}:\n\tURL: ${blockBlobClient.url}`
-    );
-  
-    // Upload data to the blob
-    const data = value;
-    // const uploadBlobResponse = await blockBlobClient.uploadData(data, {
-    //     blobHTTPHeaders: { blobContentType: 'image/jpeg' },
-    //   });
-    // console.log(
-    //   `Blob was uploaded successfully. requestId: ${uploadBlobResponse.requestId}`
-    // );
-  } catch (err: any) {
-    console.log(`Error: ${err.message}`);
-  }
-}
-
-
+import { BlobServiceClient } from "@azure/storage-blob";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const imgValue:HttpRequestBody = (req.query.img || (req.body && req.body.img));
-    
-    uploadBlob(imgValue)
-    .then(() => console.log('done'))
-    .catch((ex) => console.log(ex.message));
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: "success"
-    };
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    // if (req.query.fileType || ((req.body && req.body.image) || req.body.fileType)) {
+    if (req.body && req.body.image) {
+
+        const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+        // どのコンテナを使うかを決める
+        const containerName = process.env.AZURE_STORAGE_CONTAINER_NAME;
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        // ファイル名を決める
+        const blobName:string = 'image-' + Date.now().toString() + ".jpeg";
+        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+        // Base64エンコードされたデータを取得する
+        const data = req.body.image;
+        if (data) {
+            console.log("data is not null");
+        }
+        
+        // Base64エンコードされたデータをblobにアップロードする
+        const imageData = Buffer.from(data, 'base64');
+        await blockBlobClient.upload(imageData, imageData.length);
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Image uploaded"
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass an image in the request body"
+        };
+    }
 
 };
 
