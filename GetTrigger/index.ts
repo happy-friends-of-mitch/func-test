c1d0de86b62a4c5332eef5d3296ef477cd350480
Yanai1005 <tatsuya871_1005@icloud.com>
2023-07-16 18:45:31 +0900

    大元のget

--- GetTrigger/index.ts
+++ GetTrigger/index.ts
@@ -1,17 +1,39 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
-};
-
-export default httpTrigger;
+        const mysql = require('mysql2/promise');
+        const fs = require('fs');
+        const exsist = fs.existsSync("DigiCertGlobalRootCA.crt.pem")
+        context.log(exsist)
+        context.log(process.env["MYSQL_HOST"])
+        var config =
+        {
+            host: process.env["MYSQL_HOST"],
+            user: process.env["MYSQL_USER"],
+            password: process.env["MYSQL_PASSWORD"],
+            database: process.env["MYSQL_DB"],
+            port: 3306,
+            ssl: {ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}
+        };
+        // threadテーブルからthread_idとthread_nameをJSONファイルとして出力
+        const conn = await mysql.createConnection(config);
+        const [rows, fields] = await conn.execute(
+            'SELECT thread_id, thread_name FROM thread WHERE thread_id = 1 ORDER BY thread_id'
+            );
+            const thread = JSON.stringify(rows);
+            context.log(thread);
+            fs.writeFileSync('thread.json', thread);
+            context.log('thread.jsonにthreadテーブルのデータを出力しました。');
+    //megaテーブルからimg_url,reply_id,thread_id,threadテーブルからthread_nameをJSONファイルとして出力
+    const [rows2, fields2] = await conn.execute(
+        'SELECT mega.img_url, mega.reply_id, mega.thread_id FROM mega WHERE id = 1 '
+        // 'SELECT mega.img_url, mega.reply_id, mega.thread_id, thread.thread_name FROM mega INNER JOIN thread ON mega.thread_id = thread.thread_id'
+        );
+        const mega = JSON.stringify(rows2);
+        fs.writeFileSync('mega.json', mega);
+        context.log('mega.jsonにmegaテーブルのデータを出力しました。');  
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "OK"
+        };
+    }
