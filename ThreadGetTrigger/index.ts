45c5d14ddb75cb478a6118180ecee460e708165e
Yanai1005 <tatsuya871_1005@icloud.com>
2023-07-17 10:52:49 +0900

    fix:threadget

--- ThreadGetTrigger/index.ts
+++ ThreadGetTrigger/index.ts
@@ -15,37 +15,15 @@ module.exports = async function (context, req) {
             port: 3306,
             ssl: {ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}
         };
-        // frontからリクエストがthread_idの飛んできたらそのthread_idのmegaテーブルからimg_url,reply_id,thread_id,threadテーブルからthread_nameをJSONファイルとして出力
-        if (req.query.thread_id || (req.body && req.body.thread_id)) {
-            const thread_id = (req.query.thread_id || req.body.thread_id);
-            context.log(thread_id);
-            const conn = await mysql.createConnection(config);
-            const [rows, fields] = await conn.execute(
-                'SELECT thread_id, thread_name FROM thread WHERE thread_id = ? ORDER BY thread_id',
-                [thread_id]
-                );
-                const thread = JSON.stringify(rows);
-                context.log(thread);
-                context.log('thread.jsonにthreadテーブルのデータを出力しました。');
-        // threadテーブルからthread_idとthread_nameをJSONファイルとして出力
-        const conn2 = await mysql.createConnection(config);
-        const [rows2, fields2] = await conn.execute(
-            'SELECT thread_id, thread_name FROM thread ORDER BY thread_id'
+        const conn = await mysql.createConnection(config);
+        const [rows, fields] = await conn.execute(
+            'SELECT thread_id, thread_name FROM thread  ORDER BY RAND()'
             );
-            const thread2 = JSON.stringify(rows);
+            const thread = JSON.stringify(rows);
             context.log(thread);
             context.log('thread.jsonにthreadテーブルのデータを出力しました。');
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: thread2
-        };
-        }
-        //例外処理を書いてください
-        else {
-            context.log('thread_idがありません。');
             context.res = {
-                status: 500,
-                body: "thread_idがありません。"
+                // status: 200, /* Defaults to 200 */
+                body: thread
             };
-        }
-    }
+        };
